bf61a81e1a9a2ae92350f751f0c3275b49f1634b
Avilon <70474275+Slice3m@users.noreply.github.com>
2022-12-10 04:51:10 +0100

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -182,7 +182,7 @@ local SaveManager = {} do
 
 			local success, err = self:Load(name)
 			if not success then
-				return self.Library:Notify('Failed to load autoload config: ' .. err)
+				return self.Library
 			end
 
 			self.Library
@@ -204,15 +204,15 @@ local SaveManager = {} do
 			local name = Options.SaveManager_ConfigName.Value
 
 			if name:gsub(' ', '') == '' then 
-				return self.Library:Notify('Invalid config name (empty)', 2)
+				return self.Library
 			end
 
 			local success, err = self:Save(name)
 			if not success then
-				return self.Library:Notify('Failed to save config: ' .. err)
+				return self.Library
 			end
 
-			self.Library:Notify(string.format('Created config %q', name))
+			self.Library
 
 			Options.SaveManager_ConfigList.Values = self:RefreshConfigList()
 			Options.SaveManager_ConfigList:SetValues()
@@ -222,10 +222,10 @@ local SaveManager = {} do
 
 			local success, err = self:Load(name)
 			if not success then
-				return self.Library:Notify('Failed to load config: ' .. err)
+				return self.Library
 			end
 
-			self.Library:Notify(string.format('Loaded config %q', name))
+			self.Library
 		end)
 
 		section:AddButton('Overwrite config', function()
@@ -233,17 +233,17 @@ local SaveManager = {} do
 
 			local success, err = self:Save(name)
 			if not success then
-				return self.Library:Notify('Failed to overwrite config: ' .. err)
+				return self.Library
 			end
 
-			self.Library:Notify(string.format('Overwrote config %q', name))
+			self.Library
 		end)
 		
 		section:AddButton('Autoload config', function()
 			local name = Options.SaveManager_ConfigList.Value
 			writefile(self.Folder .. '/settings/autoload.txt', name)
 			SaveManager.AutoloadLabel:SetText('Current autoload config: ' .. name)
-			self.Library:Notify(string.format('Set %q to auto load', name))
+			self.Library
 		end)
 
 		section:AddButton('Refresh config list', function()
